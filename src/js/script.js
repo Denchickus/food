c58f507bf30171c2224385a86942f139d8fb6c13
Denis <example@gmail.com>
2022-01-05 01:16:37 +0300

    edit slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.testimonial__inner').slick({
-      dots: true,
+      dots: false,
       prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt="arrow-left"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="arrow-right"></button>'
     });
